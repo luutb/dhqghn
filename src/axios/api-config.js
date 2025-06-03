cd80201ed84e346227ccef6dcd0c88607baa5fb6
luutb <nguyenbaluu0603@gmail.com>
2025-06-03 11:50:57 +0700

    update code search studentss

--- src/axios/api-config.js
+++ src/axios/api-config.js
@@ -3,7 +3,7 @@ import { getToken, removeToken } from './handle-token'; // Giả sử bạn có 
 
 const axiosInstance = axios.create({
   //http://103.75.185.135/api
-  baseURL:'http://localhost:3200', // Thay đổi thành API cơ sở của bạn
+  baseURL:'http://103.75.185.135/api', // Thay đổi thành API cơ sở của bạn
   timeout: 60000, // Thay đổi thời gian chờ nếu cần
   headers: {
     'Content-Type': 'application/json',
